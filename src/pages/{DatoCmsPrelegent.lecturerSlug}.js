ea3b3db3ccc5642eccbaf7cc2eb94562493f87a3
jakubchrapek <chrapekuba@gmail.com>
2021-08-28 11:00:51 +0200

    Lecturer subpage ready

--- src/pages/{DatoCmsPrelegent.lecturerSlug}.js
+++ src/pages/{DatoCmsPrelegent.lecturerSlug}.js
@@ -9,61 +9,94 @@ import siteConfig from "../../config/site-config"
 import Seo from "../components/Seo"
 import { GatsbyImage } from "gatsby-plugin-image"
 
+const LecturerTextStyles = styled(TextPageStyles)`
+  padding: 4rem 0 2rem;
+  @media (max-width: 767px) {
+    padding: 2.5rem 0 1rem;
+  }
+  h2 {
+    margin: 0 0 1.5rem;
+  }
+`
+
 const PartnerBio = styled.section`
   margin-bottom: 2rem;
   @media (max-width: 1024px) {
     flex-direction: column;
     margin-bottom: 8rem;
   }
+`
+
+const PartnerInfoWrapper = styled.div`
+  padding-bottom: 0.8rem;
+  display: flex;
+  @media (max-width: 962px) {
+    flex-direction: column;
+  }
   > .gatsby-image-wrapper {
-    float: left;
-    margin: 0 4rem 0.75rem 0;
-    @media (max-width: 576px) {
-      float: unset;
-      margin-right: 0;
-    }
     display: inline-flex;
     justify-content: center;
     align-items: center;
-    width: 22rem;
-    height: 22rem;
-    border-radius: 22rem;
-    padding: 4rem;
+    width: 20vw;
+    height: 20vw;
+    max-width: 19.75rem;
+    max-height: 19.75rem;
+    min-width: 18rem;
+    min-height: 18rem;
+    border-radius: 20vw;
+
+    img {
+      width: 20vw;
+      height: 20vw;
+      max-width: 19.75rem;
+      max-height: 19.75rem;
+      min-width: 18rem;
+      min-height: 18rem;
+      border-radius: 20vw;
+    }
 
     @media (max-width: 1200px) {
-      width: 18rem;
-      height: 18rem;
+      max-width: 18rem;
+      max-height: 18rem;
       border-radius: 18rem;
-      padding: 3rem;
+      img {
+        max-width: 18rem;
+        max-height: 18rem;
+        border-radius: 18rem;
+      }
     }
     @media (max-width: 1024px) {
       width: 55vw;
       height: 55vw;
       max-width: 13rem;
       max-height: 13rem;
-      padding: 2rem;
+      img {
+        max-width: 13rem;
+        max-height: 13rem;
+        border-radius: 13rem;
+      }
     }
-    @media (max-width: 520px) {
-      width: 75vw;
-      height: 75vw;
-      max-width: 460px;
-      max-height: 460px;
-      padding: 0;
+    @media (max-width: 380px) {
+      max-width: 100%;
+      max-height: 100%;
+      min-width: 220px;
+      min-height: 220px;
+      width: 100%;
+      padding-bottom: 100%;
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        min-width: 220px;
+        min-height: 220px;
+        width: 100%;
+        height: 100%;
+      }
     }
-  }
-`
-
-const PartnerInfoWrapper = styled.div`
-  padding-bottom: 0.8rem;
-  @media (max-width: 1200px) {
-    flex: 1 1 60%;
-  }
-  @media (max-width: 1024px) {
-    margin-top: 2.125rem;
   }
 `
 
 const BackToLecturers = styled(Link)`
+  margin-top: 2rem;
   color: currentColor;
   font-size: var(--font-20);
   @media (max-width: 767px) {
@@ -71,6 +104,18 @@ const BackToLecturers = styled(Link)`
   }
   font-weight: bold;
   line-height: 1.7;
+`
+
+const ContentWrapper = styled.div`
+  flex: 1 1 70%;
+  margin: 0.5rem 0 0 4rem;
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: 962px) {
+    margin: 2.5rem 0 0;
+    flex: 1 1 100%;
+  }
 `
 
 const PartnerPage = ({ data }) => {
@@ -81,26 +126,30 @@ const PartnerPage = ({ data }) => {
         description={siteConfig.description}
         meta={data.datoCmsPageHome.seoMetaTags}
       />
-      <TextPageStyles>
+      <LecturerTextStyles>
         <PartnerBio>
-          <GatsbyImage
-            image={data.datoCmsPrelegent.lecturerPhoto.gatsbyImageData}
-            alt={`Portret ${data.datoCmsPrelegent.lecturerNameAndSurname}`}
-          />
           <PartnerInfoWrapper>
-            <h2>{data.datoCmsPrelegent.lecturerNameAndSurname}</h2>
-            <StructuredText
-              data={data.datoCmsPrelegent.lecturerLongBiography.value}
+            <GatsbyImage
+              image={data.datoCmsPrelegent.lecturerPhoto.gatsbyImageData}
+              alt={`Portret ${data.datoCmsPrelegent.lecturerNameAndSurname}`}
             />
+            <ContentWrapper>
+              <h2>{data.datoCmsPrelegent.lecturerNameAndSurname}</h2>
+              <StructuredText
+                data={data.datoCmsPrelegent.lecturerLongBiography.value}
+              />
+              <BackToLecturers to="/#prelegenci">
+                Powrót do partnerów
+              </BackToLecturers>
+            </ContentWrapper>
           </PartnerInfoWrapper>
         </PartnerBio>
-        <BackToLecturers to="/#prelegenci">Wróć do prelegentów</BackToLecturers>
         <Circles
           plainText={data.datoCmsPageHome.additionalInfoCtaText}
           filledCircleText={data.datoCmsPageHome.filledCircleContent}
           variant="plainText"
         />
-      </TextPageStyles>
+      </LecturerTextStyles>
     </Layout>
   )
 }
